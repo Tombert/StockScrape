b90794bff5161ced5629071137f7bcbfc12c34a5
Thomas Gebert <thomasgebert@gmail.com>
2015-05-20 10:00:03 -0400

    Write system for web requests; now to just parse the JSON.

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,23 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+
 module Main  where
 import Data.List.Split 
+import Data.String
+import Network.HTTP
+import Text.JSON.Generic
+
+
+data Stock = Stock 
+     {
+       symbol :: String,
+       daysHigh :: String,
+       daysLow  :: String
+     }
+
 
 quoteWrap x = "\"" ++ x ++ "\""
 
-breakAndChunk = (splitEvery 100) . (map quoteWrap) . lines
+breakAndChunk = (splitEvery 250) . (map quoteWrap) . lines
 
 
 commaList :: [String] -> String
@@ -12,17 +26,26 @@ commaList = tail . (foldl concatFields "")
 parenWrap :: String -> String
 parenWrap x = "(" ++ x ++ ")"
 
-queryBuilder x = "select * from yahoo.finance.quote where symbol in " ++ x
+queryBuilder x = "use \"http://github.com/spullara/yql-tables/raw/d60732fd4fbe72e5d5bd2994ff27cf58ba4d3f84/yahoo/finance/yahoo.finance.quotes.xml\" as quotes; select * from quotes where symbol in " ++ x
 
-makeQuery = queryBuilder . parenWrap . commaList
+urlBuilder x = "http://query.yahooapis.com/v1/public/yql?format=json&q=" ++ x
+
+makeUrl = urlBuilder . urlEncode . queryBuilder . parenWrap . commaList
 
 concatFields :: String -> String -> String
 concatFields x y = x ++ "," ++ y
+
+get :: String -> IO String
+get url = simpleHTTP (getRequest url) >>= getResponseBody
 
 main :: IO ()
 main = do 
          symbolsFile <- readFile "symbols.csv"
          let symbols = breakAndChunk symbolsFile
-         let queries = map makeQuery symbols
-
-         putStrLn (queries!!0) 
+         let urls = map makeUrl symbols
+         let responses = map get urls
+         --lah <- map (>>=) responses
+         mapM_ (>>=print) responses 
+         --respgonse <- het ("http://query.yahooapis.com/v1/public/yql?format=json&amp;q=" ++ (queries !! 0))
+         --
+         --putStrLn (queries !! 0) 
